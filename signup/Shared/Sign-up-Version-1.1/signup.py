635e33d6fbdc70f35a5ab2d36a91b4ffc73a6bdc
LFox42 <47540294+LFox42@users.noreply.github.com>
2019-03-13 14:58:53 +1300

    Delete signup.py

--- signup/Shared/Sign-up-Version-1.1/signup.py
+++
@@ -1,6 +0,0 @@
-
-tickets = [
-    ticket("Liam fox", "liamfox@my.rnls.school.nz", "28.11.2001"),
-    ticket("Jeremy roberts", "jeremyroberts@my.rnls.school.nz", "??.??.????"),
-    ticket("Moses Wescombe", "liamfox@my.rnls.school.nz", "??.??.????"),
-    ticket("Liam Chessum", "liamfchessum@my.rnls.school.nz", "??.??.????")]
